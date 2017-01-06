a88087d2765a58dbbfa6b09e7b8ee4cbcb2c8b50
Kris Sparks <kbs5280@gmail.com>
2017-01-06 10:56:57 -0700

    Update test to increase quantity on snack object

--- test/snack_test.rb
+++ test/snack_test.rb
@@ -7,6 +7,6 @@ class SnackTest < Minitest::Test
   def test_snack_can_be_any_snack
     snack = Snack.new("White Castle Burger", 3)
     assert_equal "White Castle Burger", snack.name
-    assert_equal 3, snack.quantity 
+    assert_equal 3, snack.quantity
   end
 end
